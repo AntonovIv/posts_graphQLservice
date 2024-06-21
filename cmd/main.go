e03ef92cfd64fdfee4109deefe9a0c86425dc75f
AntonovIv <vance7887@gmail.com>
2024-06-21 15:21:48 +0300

    working test server

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/AntonovIv/post_graphQlservice/graph"
+
+	graphTr "github.com/AntonovIv/post_graphQlservice/internal/transport/graph"
 )
 
 func main() {
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema())
+
+	port := "8080"
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graphTr.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
